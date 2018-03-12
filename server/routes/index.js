a5288478645c84c96c1b9963fb69c3fbcadb2326
Josh Sonstroem <jsonstro@ucsc.edu>
2018-03-11 22:53:25 -0400

    Adding admin pages

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,33 +7,36 @@ module.exports = (app) => {
   app.get('/', (req, res) => {  
     res.render('home', {})
   })
+  app.get('/admin', (req, res) => {  
+    res.render('admin', {})
+  })
 
   app.get('/api', (req, res) => res.status(200).send({
     message: 'Welcome to the InBoard Customer Service API!',
   }));
 
   app.post('/api/data', dataController.create);
-  app.post('/api/user', userController.create);
   app.post('/api/rush', rushController.create);
+  app.post('/api/user', userController.create);
   app.post('/api/version', versController.create);
   app.post('/api/fw', versController.createFw);
   app.post('/api/bcu', versController.createBcu);
 
   app.get('/api/data', dataController.list);
-  app.get('/api/user', userController.list);
   app.get('/api/rush', rushController.list);
+  app.get('/api/user', userController.list);
   app.get('/api/version', versController.list);
   app.get('/api/fw', versController.listFw);
   app.get('/api/bcu', versController.listBcu);
 
-  app.get('/api/rush/:deck_sn', rushController.retrieve);
   app.get('/api/data/:deck_sn', dataController.retrieve);
+  app.get('/api/rush/:deck_sn', rushController.retrieve);
 
-  app.post('/api/form_r', rushController.fetch);
   app.post('/api/form_d', dataController.fetch);
+  app.post('/api/form_r', rushController.fetch);
 
-  app.post('/api/import_r', rushController.csvImport);
-  app.post('/api/upport_r', rushController.csvUpport);
   app.post('/api/import_d', dataController.gshImport);
   app.post('/api/import_p', dataController.csvImport);
+  app.post('/api/import_r', rushController.csvImport);
+  app.post('/api/upport_r', rushController.csvUpport);
 };
